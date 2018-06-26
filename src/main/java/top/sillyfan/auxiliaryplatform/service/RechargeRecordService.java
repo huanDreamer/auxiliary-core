0dc1bfb3dcc69f16d8b175c2380beef232c9bb9a
huan <huan.dreamer@gmail.com>
2018-06-26 23:41:33 +0800

    充值记录

--- src/main/java/top/sillyfan/auxiliaryplatform/service/RechargeRecordService.java
+++ src/main/java/top/sillyfan/auxiliaryplatform/service/RechargeRecordService.java
@@ -1,6 +1,7 @@
 package top.sillyfan.auxiliaryplatform.service;
 
 import top.sillyfan.auxiliaryplatform.common.BaseService;
+import top.sillyfan.auxiliaryplatform.constants.RechargeRecordDef;
 import top.sillyfan.auxiliaryplatform.domain.model.RechargeRecord;
 import top.sillyfan.auxiliaryplatform.domain.model.repository.RechargeRecordMapper;
 
@@ -8,6 +9,6 @@ import java.math.BigDecimal;
 
 public interface RechargeRecordService extends BaseService<RechargeRecord, Long, RechargeRecordMapper> {
 
-    void saveRecord(Long operatorId, Long demanderId, BigDecimal oldBalance, BigDecimal newBalance);
+    void saveRecord(Long operatorId, Long demanderId, RechargeRecordDef.RechargeRecordTypeEnum type, BigDecimal oldBalance, BigDecimal newBalance);
 
 }
--- src/main/java/top/sillyfan/auxiliaryplatform/service/impl/RechargeRecordServiceImpl.java
+++ src/main/java/top/sillyfan/auxiliaryplatform/service/impl/RechargeRecordServiceImpl.java
@@ -2,9 +2,9 @@ package top.sillyfan.auxiliaryplatform.service.impl;
 
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Service;
 import top.sillyfan.auxiliaryplatform.common.BaseServiceImpl;
+import top.sillyfan.auxiliaryplatform.constants.RechargeRecordDef;
 import top.sillyfan.auxiliaryplatform.domain.model.RechargeRecord;
 import top.sillyfan.auxiliaryplatform.domain.model.repository.RechargeRecordMapper;
 import top.sillyfan.auxiliaryplatform.service.RechargeRecordService;
@@ -22,24 +22,25 @@ public class RechargeRecordServiceImpl extends BaseServiceImpl<RechargeRecord, L
     }
 
     @Override
-    public void saveRecord(Long operatorId, Long demanderId, BigDecimal oldBalance, BigDecimal newBalance) {
+    public void saveRecord(Long operatorId, Long demanderId, RechargeRecordDef.RechargeRecordTypeEnum type, BigDecimal oldBalance, BigDecimal newBalance) {
 
         // 金额不变
-        if(Objects.isNull(newBalance)) {
+        if (Objects.isNull(newBalance)) {
             return;
         }
 
-        if(Objects.isNull(oldBalance)) {
+        if (Objects.isNull(oldBalance)) {
             oldBalance = BigDecimal.ZERO;
         }
 
-        if(oldBalance.compareTo(newBalance) == 0) {
+        if (oldBalance.compareTo(newBalance) == 0) {
             return;
         }
 
         // 保存记录
         RechargeRecord rechargeRecord = new RechargeRecord();
 
+        rechargeRecord.setType(type.getCode());
         rechargeRecord.setDemanderId(demanderId);
         rechargeRecord.setOperator(operatorId);
         rechargeRecord.setMoney(newBalance.subtract(oldBalance));
